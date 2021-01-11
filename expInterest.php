1803954a919e634558fcc1a70dab10ac13eaca14
Tracy <57853991+TracyWynter@users.noreply.github.com>
2021-01-11 22:40:55 +0800

    File Organize (Tracy)

---
+++ expInterest.php
@@ -0,0 +1,12 @@
+<?php
+
+//information to collect:
+//name
+//telephone number
+//email
+//product id
+//proposing price
+
+
+
+
---
+++ myMusicGear.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>My Music Gear</title>
+        <style type="text/css">
+            form.search input[type=text]{
+                width: 60%;
+                height: 30px;
+            }
+            form.search button {
+                height: 30px;
+            }
+         </style>
+    </head>
+    
+    <body>
+        <h1><center> Welcome to My Music Gear </center></h1>
+
+        <p><center>What are you looking for?</center></p>
+        <form class="search" action="/search.php"><center>
+                <input type="text" placeholder="Example:Guitar / Piano / Ukelele .... " name="search">
+                <button type="submit"><i class="fa fa-search">Search</i></button>
+            </center></form>
+
+    </body>
+</html>
